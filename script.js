60be58bfd4fe5bcbaddfae321c32834bd957160d
arthagp <arthagusfi11@gmail.com>
2023-01-01 17:35:24 +0700

    updated

--- script.js
+++ script.js
@@ -20,3 +20,61 @@ const showSlide = (n) => {
 }
 
 showSlide(slideIndex);
+
+
+// Ketika tombol openButton ditekan
+function btnClick() {
+
+  document.getElementById("myModal").style.display = "block";
+  console.log("work");
+}
+
+// Ketika tombol closeButton ditekan
+function closeClick() {
+
+  document.getElementById("myModal").style.display = "none";
+  console.log("work");
+}
+
+// alertt
+const inputEmail = document.getElementById("mail");
+const textArea = document.getElementById("txtArea");
+
+function refresh() {
+  mail.value = "";
+  txtArea.value = "";
+}
+function require() {
+  if (mail.value && txtArea.value) {
+    refresh();
+    alert('Berhasil');
+    // return;
+  } else if (mail.value === "" && txtArea.value === "") {
+    refresh();
+    alert('E-mail dan Message tidak boleh kosong')
+  } else if (mail.value == "") {
+    refresh();
+    alert('E-mail tidak boleh kosong')
+  } else if (txtArea.value === "") {
+    refresh();
+    alert('Message tidak boleh kosong')
+  }
+}
+
+// ACCORDION
+
+  const accordion = document.getElementsByClassName("accordion");
+
+  for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function () {
+      const panel = this.nextElementSibling;
+
+      if (panel.style.display === "block") {
+        console.log(panel)
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
+
